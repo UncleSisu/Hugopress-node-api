411f454b4a80e40810996a3bf09b1a8ad20aec08
Blake Watkins <iamsoblake@gmail.com>
2017-11-08 18:13:53 -0800

    Server now accommodates build commands

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
+let { exec } = require('child_process');
 
 // Constants
 const PORT = 3000;
@@ -8,8 +10,33 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
+  console.log(`Derpitude http://${HOST}:${PORT}`);
   res.send('Hello world\n');
+});
+
+// app.get('/wp-hugo', (req, res) => {
+//   console.log(`Huuuuuugo`, req.body);
+//   res.send('Hello Hugo!\n');
+// });
+
+app.post('/wp-hugo', (req, res) => {
+  const cmd = JSON.parse(req.body.payload);
+  if (cmd.text === 'build') {
+    console.log('check dir', __dirname);
+
+    let cmd = `cd hugo-static-builder-module
+    yarn hugo-test `
+    exec(cmd , (err, stdout, stderr) => {
+      console.log(`Huuuuuugo`, stdout);
+      res.send(stdout);
+    });
+  } else {
+    console.log(`Huuuuuugo no build today`);
+    res.send(cmd.text);
+  }
 });
 
 app.listen(PORT, HOST);
