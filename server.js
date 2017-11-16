aeab03ea77c4be785c95b9c4d01dc9405b981990
Blake Watkins <iamsoblake@gmail.com>
2017-11-15 17:30:16 -0800

    Log actions from wp. Start trying wpapi npm module

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 let { exec } = require('child_process');
 
+// TODO: connect wpapi to database
+// const WPAPI = require('wpapi');
+// const wp = new WPAPI({ endpoint: 'http://localhost/starter/wordpress/wp-json/wp/v2/' });
+// const apiPromise = WPAPI.discover('http://localhost/starter/');
+//
+// apiPromise.then(function(site) {
+//   site.posts().then(function(posts) {
+//     console.log('see them posts', posts);
+//   })
+// })
+
 // Constants
 const PORT = 3000;
 const HOST = '0.0.0.0';
@@ -23,24 +34,38 @@ app.get('/', (req, res) => {
 // });
 
 app.post('/wp-hugo', (req, res) => {
-  const cmd = JSON.parse(req.body.payload);
-  if (cmd.text === 'build') {
-    console.log('check dir', __dirname);
-
-    // yarn hugo-test
-    let cmd = `
-    cd hugo-static-builder-module
-    yarn build
-    ls -la
-    `;
-    exec(cmd , (err, stdout, stderr) => {
-      console.log(`Huuuuuugo`, stdout);
-      res.send(stdout);
-    });
-
-  } else {
-    console.log(`Huuuuuugo no build today`);
-    res.send(cmd.text);
+  const wpInstructions = JSON.parse(req.body.payload);
+  switch (wpInstructions.text) {
+    case 'build-page':
+      // let command = `
+      // cd hugo-static-builder-module
+      // yarn build
+      // ls -la
+      // `;
+      // exec(command , (err, stdout, stderr) => {
+      //   console.log(`Huuuuuugo`, stdout);
+      //   res.send(stdout);
+      // });
+      console.log(`Hugo post build-page`, wpInstructions);
+      res.send(wpInstructions);
+      break;
+    case 'build-generic':
+      // let command = `
+      // cd hugo-static-builder-module
+      // yarn build
+      // ls -la
+      // `;
+      // exec(command , (err, stdout, stderr) => {
+      //   console.log(`Huuuuuugo`, stdout);
+      //   res.send(stdout);
+      // });
+      console.log(`Hugo post build-generic`, wpInstructions);
+      res.send(wpInstructions);
+      break;
+    default:
+      console.log(`Hugo post default`, wpInstructions);
+      res.send(wpInstructions);
+      break;
   }
 });
 
